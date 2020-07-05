cfdd605989bae148101bb0b9ce8b770cc0d71af6
Shivaraj <ashivaraj@deloitte.com>
2020-07-05 22:34:56 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -4,3 +4,26 @@ import MyApp from 'my/app';
 const app = createElement('my-app', { is: MyApp });
 // eslint-disable-next-line @lwc/lwc/no-document-query
 document.querySelector('#main').appendChild(app);
+
+const jsforce = require('jsforce');
+const conn = new jsforce.Connection({
+    // you can change loginUrl to connect to sandbox or prerelease env.
+    loginUrl: 'https://login.salesforce.com'
+});
+conn.login('anil.shivaraj@gmail.com.personal', 'Welcome#123456', function (
+    err,
+    userInfo
+) {
+    if (err) {
+        return console.error(err);
+    }
+    // Now you can get the access token and instance URL information.
+    // Save them to establish connection next time.
+    console.log(conn.accessToken);
+    console.log(conn.instanceUrl);
+    // logged in user property
+    console.log('User ID: ' + userInfo.id);
+    console.log('Org ID: ' + userInfo.organizationId);
+    // ...
+    return userInfo;
+});
